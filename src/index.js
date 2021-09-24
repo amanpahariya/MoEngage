986848ce4b3cad2a8a8846e5da6ec01999552941
aman <amangupta2786@gmail.com>
2021-09-25 02:16:36 +0530

    add procfile and remove mdbuicss from index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'mdb-react-ui-kit/dist/css/mdb.min.css'
+// import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 import './index.css';
 import store from "./store";
 import {Provider} from "react-redux";
